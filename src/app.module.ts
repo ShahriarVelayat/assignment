a00015682c8c9e26ee13dcbc31092ea94ed50f05
Shahriar Velayat <shahriarvelayat@gmail.com>
2025-07-24 16:07:42 +0330

    add entities
    define separate entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { JobModule } from './job/job.module';
+import { CompanyModule } from './company/company.module';
+import { LocationModule } from './location/location.module';
+import { RequirementModule } from './requirement/requirement.module';
 import * as configs from 'config'
 
 let database_config:any = configs.get('database')
@@ -17,6 +21,10 @@ let database_config:any = configs.get('database')
       entities: [],
       synchronize: true,
     }),
+    JobModule,
+    CompanyModule,
+    LocationModule,
+    RequirementModule,
   ],
   controllers: [AppController],
   providers: [AppService],
