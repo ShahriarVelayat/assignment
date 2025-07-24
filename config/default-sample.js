980186752ee9d9e7c3a3cb7c02c49f1021f3a3a6
Shahriar Velayat <shahriarvelayat@gmail.com>
2025-07-24 15:38:00 +0330

    add database config file

---
+++ config/default-sample.js
@@ -0,0 +1,12 @@
+let config = {
+    environment:'development',
+    base_url: 'http://localhost',
+    port: 3000,
+    database:{
+        host:'localhost',
+        port:3306,
+        username: 'root',
+        password: 'password',
+        database: 'test'
+    }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import * as configs from 'config';
 
+let database_config = configs.get('database')
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: database_config.host,
+      port: database_config.port,
+      username: database_config.username,
+      password: database_config.password,
+      database: database_config.database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+}
